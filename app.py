4130b23497e67b3a9becc5a4175aeb47959f6dfe
Gaurisha21 <68791455+Gaurisha21@users.noreply.github.com>
2021-08-18 22:44:49 +0530

    updated app.py

--- app.py
+++ app.py
@@ -86,8 +86,8 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+	return '''<h1>Suggestions to deal with Anger Management issues api</h1>
+<p>A prototype API for providing solutions to deal with Anger Management issues.</p>'''
 
 
 @app.route('/api/anagersolution/all', methods=['GET'])
